aadae55c2f87938dae6654d6895730492cd5c696
KalariaYash <yash.kalaria1998@gmail.com>
2019-05-24 11:44:53 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Text } from "native-base";
+
+const App = () => {
+    return (
+      <Container>
+       <Text>Hello</Text>
+      </Container>
+    );
+}
+
+export default App;
